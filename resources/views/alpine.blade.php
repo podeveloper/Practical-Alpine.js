3a45eb5338ad31ac7fa89968217b585fae862afe
Yasin Korkmaz <theacherx@gmail.com>
2023-10-15 23:12:00 +0300

    Click Away Attribute

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -13,16 +13,27 @@
 <div x-data="{
         inputType: 'password',
         buttonText: 'Show',
+        showing: true,
 
         changeType()
         {
             this.inputType = (this.inputType == 'password') ? 'text' : 'password';
             this.buttonText = (this.buttonText == 'Show') ? 'Hide' : 'Show';
+        },
+
+        cardToggle()
+        {
+            this.showing = !this.showing;
         }
     }">
     <div class="flex h-screen">
         <div class="m-auto">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+
+            <button @click="cardToggle" x-show="!showing" class="bg-blue-500 hover:bg-blue-700 my-2 text-white font-bold py-2 px-4 rounded">
+                Show The Card
+            </button>
+
+            <div x-show="showing" class="max-w-sm rounded overflow-hidden shadow-lg bg-white" @click.away="cardToggle">
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">Password Show & Hide</div>
                     <input x-bind:type="inputType" class="rounded p-2 h-full border-b border-blue-gray-200 bg-gray-100" :placeholder="inputType == 'password' ? '*******************' : 'Type your password here'">
