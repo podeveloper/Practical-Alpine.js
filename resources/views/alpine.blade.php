298dc8f5e803fa3df897a17e9a0deccbfe84b294
Yasin Korkmaz <theacherx@gmail.com>
2023-10-15 22:54:55 +0300

    Password Toggle - Show & Hide

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -7,11 +7,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <div x-data="{open: false}">
-        <button @click="open = !open">Toggle</button>
-        <div x-show="open">Contents...</div>
+<body class="bg-gray-600">
+<div x-data="{
+        inputType: 'password',
+        buttonText: 'Show',
+
+        changeType()
+        {
+            this.inputType = (this.inputType == 'password') ? 'text' : 'password';
+            this.buttonText = (this.buttonText == 'Show') ? 'Hide' : 'Show';
+        }
+    }">
+    <div class="flex h-screen">
+        <div class="m-auto">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Password Show & Hide</div>
+                    <input x-bind:type="inputType" class="rounded p-2 h-full border-b border-blue-gray-200 bg-gray-100" placeholder="Type your password here">
+                    <button @click="changeType" class="my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" x-text="buttonText"></button>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 </body>
 </html>
